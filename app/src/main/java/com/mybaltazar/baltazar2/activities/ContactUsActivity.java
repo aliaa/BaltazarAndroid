a172947153d2fa98b2be162621f4c1c934a95dbf
Ali Aboutalebi <aboutalebi@live.com>
2019-03-02 00:41:15 +0330

    .

---
+++ app/src/main/java/com/mybaltazar/baltazar2/activities/ContactUsActivity.java
@@ -0,0 +1,18 @@
+package com.mybaltazar.baltazar2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mybaltazar.baltazar2.R;
+
+public class ContactUsActivity extends BaseActivity {
+
+    public ContactUsActivity() {
+        super(R.layout.activity_contact_us, true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
